a3d4a8a71c93f73bb49520988730fe8ef0997378
0Yasser <yasiirh@gmail.com>
2021-10-07 14:02:13 +0300

    First Commit

---
+++ W01D05/myVariables.js
@@ -0,0 +1,15 @@
+let language = "JavaScript",
+  score = 10;
+console.log(language + "\n" + score);
+
+let length = 20,
+  width = 10;
+console.log("The area of the rectangle is " + width * length);
+
+let num1 = 5,
+  num2 = 3;
+console.log(num1 % num2);
+
+console.log(
+  "num1 / num2 = " + Math.trunc(num1 / num2) + " remainder " + (num1 % num2)
+);
